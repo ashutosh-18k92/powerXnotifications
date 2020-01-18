eaf8476e8e8f49a1e1cd59c0e56036563e1617d7
ashutosh-18k92 <ashutosh.18k92@outlook.com>
2020-01-18 16:48:32 +0530

    Added: Backend folder with email microservice

---
+++ BackEnd/EmailBroadcast/src/main/java/com/powerxnotifications/emailsvc/EmailBroadcast/EmailBroadcastApplication.java
@@ -0,0 +1,13 @@
+package com.powerxnotifications.emailsvc.EmailBroadcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailBroadcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailBroadcastApplication.class, args);
+	}
+
+}
---
+++ BackEnd/EmailBroadcast/src/test/java/com/powerxnotifications/emailsvc/EmailBroadcast/EmailBroadcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerxnotifications.emailsvc.EmailBroadcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailBroadcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
